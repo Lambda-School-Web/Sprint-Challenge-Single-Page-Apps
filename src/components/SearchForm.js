ff2aaa9b026bf89f62357e109df6b6e5777127bb
jambis <imjaamees@gmail.com>
2019-09-20 11:08:21 -0500

    Added Tabs navigation in Header component

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
+import { withFormik, Form, Field } from "formik";
 
 export default function SearchForm() {
- 
-  return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
-  );
+  return <section className="search-form">// Add a search form here</section>;
 }
